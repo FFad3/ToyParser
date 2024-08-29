b493ebc1dd437a8ce20778f2853910b3a273b273
FFad3 <gnebapatryk@gmail.com>
2024-08-29 23:05:59 +0200

    Implement Lexer

---
+++ ToyParser.Lexer/Lexer.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+using ToyParser.Utils;
+
+namespace ToyParser.Lexer
+{
+    public class Lexer : ILexer
+    {
+        private readonly ReadOnlyMemory<char> _content;
+        private readonly TokenType[] _ignoredTokens;
+        private static readonly Dictionary<Regex, TokenType> _regexToTokenType;
+        private int _position;
+        private bool _isEOF => this._position >= this._content.Length;
+        private ReadOnlyMemory<char> _currentSlice => this._content[this._position..];
+
+        static Lexer()
+        {
+            _regexToTokenType = new()
+            {
+                {RegexPaterns.ReturnRegex, TokenType.RETURN },
+            };
+        }
+
+        public Lexer(ReadOnlyMemory<char> content, TokenType[] ignoredTokens)
+        {
+            this._position = 0;
+            this._content = content;
+            this._ignoredTokens = ignoredTokens;
+        }
+
+        public Token GetToken()
+        {
+            Position position = new Position(this._position, this._content.Length);
+
+            if (_isEOF)
+                return new Token(TokenType.EOF, this._currentSlice, new());
+
+            foreach ((Regex regex, TokenType tokenType) in _regexToTokenType)
+            {
+                Match match = regex.Match(this._currentSlice);
+                if (match.Success)
+                {
+                    position = new Position(this._position, this._position + match.Length);
+
+                    return tokenType switch
+                    {
+                        _ => new Token(tokenType, this._content.Slice(this._position, match.Length), position)
+                    };
+                }
+            }
+            return new Token(TokenType.ILLEGAL, this._currentSlice, position);
+        }
+
+        public Token GetNextToken()
+        {
+            Token token = GetToken();
+            if (_ignoredTokens.Contains(token.Type))
+            {
+                token = GetToken();
+            }
+            this._position += token.Position.Length;
+            return token;
+        }
+    }
+}
---
+++ ToyParser.Utils/RegexExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ToyParser.Utils
+{
+    public static class RegexExtension
+    {
+        public static Match Match(this Regex regex, ReadOnlyMemory<char> readOnlyMemory)
+        {
+            string content = new string(readOnlyMemory.Span);
+            return regex.Match(readOnlyMemory.ToString());
+        }
+    }
+}
