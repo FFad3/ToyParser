5c5c4d42a66b2e2c266cac111f51cd1c29757dcf
FFad3 <gnebapatryk@gmail.com>
2024-08-29 21:22:15 +0200

    Add TokenType enum

---
+++ ToyParser.Lexer/TokenType.cs
@@ -0,0 +1,37 @@
+﻿namespace ToyParser.Lexer
+{
+    public enum TokenType
+    {
+        EOF,
+        ILLEGAL,
+        IDNETIFIER,
+        DIGIT,
+
+        ASSIGN,
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE,
+
+        EQUALS,
+        NOT_EQUALS,
+        LESS_THAN,
+        GREATER_THAN,
+
+        SEMICOLON,
+        COMMA,
+        DOT,
+        EXCLAMATION,
+
+        L_PAREN,
+        R_PAREN,
+
+        L_BRACE,
+        R_BRACE,
+
+        TRUE,
+        FALSE,
+
+        RETURN,
+    }
+}
