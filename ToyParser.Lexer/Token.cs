01649e49c64dfbf9037b0b33a96d65a8d58f142d
FFad3 <gnebapatryk@gmail.com>
2024-08-29 21:22:43 +0200

    Add Token struct

---
+++ ToyParser.Lexer/Token.cs
@@ -0,0 +1,10 @@
+﻿namespace ToyParser.Lexer
+{
+
+    public readonly struct Token(TokenType type, ReadOnlyMemory<char> value, Position position)
+    {
+        public TokenType Type { get; } = type;
+        public ReadOnlyMemory<char> Value { get; } = value;
+        public Position Position { get; } = position;
+    }
+}
