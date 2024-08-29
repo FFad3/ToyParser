5546d3ec04578f9f7da7629ccb816e20ba128a62
FFad3 <gnebapatryk@gmail.com>
2024-08-29 19:54:47 +0200

    Add project files.

---
+++ ToyParser.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToyParser.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ToyParser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToyParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
