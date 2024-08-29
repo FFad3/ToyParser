06a72d57106869b111fec035bcb0af4d3c0bfe3f
FFad3 <gnebapatryk@gmail.com>
2024-08-29 21:22:33 +0200

    Add Position struct

---
+++ ToyParser.Lexer/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace ToyParser.Lexer
+{
+    public readonly struct Position(int start, int end)
+    {
+        public int Start { get; } = start;
+        public int End { get; } = end;
+        public int Length => End - Start;
+
+        public override string ToString()
+        {
+            return $"POSITION: [{Start},{End}] LENGHT: {Length}";
+        }
+    }
+}
