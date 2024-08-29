cfa35c0233629ca070fa3211b05e9159dac97421
FFad3 <gnebapatryk@gmail.com>
2024-08-29 23:05:23 +0200

    Add Regex Patterns

---
+++ ToyParser.Lexer/RegexPaterns.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ToyParser.Lexer
+{
+    public static class RegexPaterns
+    {
+
+        public static readonly Regex ReturnRegex = new(@"(?i)^return", RegexOptions.Compiled);
+    }
+}
