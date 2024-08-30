1bdbd5993028cde6dab4dd4cdbffc5bf222374fe
FFad3 <gnebapatryk@gmail.com>
2024-08-30 18:19:03 +0200

    Add base AST Nodes

---
+++ ToyParser/ASTnodes/ASTNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public abstract class ASTNode
+    {
+        public string Name => this.GetType().Name;
+
+        public abstract string GetValue();
+
+        public abstract IEnumerable<ASTNode> GetChildren();
+
+        public virtual void Accept(IASTVisitor visitor, bool IsLast)
+        {
+            visitor.Visit(this, IsLast);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} : {GetValue()}";
+        }
+    }
+}
---
+++ ToyParser/ASTnodes/CompilationUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class CompilationUnit(List<ASTNode> nodes) : ASTNode
+    {
+        private readonly List<ASTNode> _nodes = nodes;
+
+        public override IEnumerable<ASTNode> GetChildren() => _nodes;
+
+        public override string GetValue() => string.Empty;
+    }
+}
