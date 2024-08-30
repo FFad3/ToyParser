30707564c17296cff87558091475024ae8de2ef2
FFad3 <gnebapatryk@gmail.com>
2024-08-30 18:18:36 +0200

    Add BinaryExpression AST Node + Operator struct

---
+++ ToyParser/ASTnodes/BinaryExpression.cs
@@ -0,0 +1,21 @@
+﻿using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class BinaryExpression(ASTNode Left, Operator Operator, ASTNode Right) : ASTNode
+    {
+        public ASTNode Left { get; } = Left;
+        public Operator Operator { get; } = Operator;
+        public ASTNode Right { get; } = Right;
+
+        public override void Accept(IASTVisitor visitor, bool isLast)
+        {
+            visitor.Visit(this, isLast);
+        }
+
+        public override IEnumerable<ASTNode> GetChildren() => [Left, Right];
+
+        public override string GetValue() => Operator.ToString();
+    }
+
+}
---
+++ ToyParser/ASTnodes/Operator.cs
@@ -0,0 +1,18 @@
+﻿using ToyParser.Lexer;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public readonly struct Operator
+    {
+        public TokenType Type { get; }
+        public string Value { get; }
+
+        public Operator(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString() => Type.ToString();
+    }
+}
