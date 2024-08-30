93d2e536f3d0feba73a0a69be2d995d2d5c60976
FFad3 <gnebapatryk@gmail.com>
2024-08-30 18:18:48 +0200

    Add AST literal nodes

---
+++ ToyParser/ASTnodes/StringLiteral.cs
@@ -0,0 +1,12 @@
+﻿namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class StringLiteral(string value) : ASTNode
+    {
+        public string Value { get; } = value;
+
+        public override IEnumerable<ASTNode> GetChildren() => [];
+
+        public override string GetValue() => Value;
+    }
+
+}
