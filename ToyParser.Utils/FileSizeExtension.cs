77dd27f852aeaa18dc969fc2d3300ed4503c317c
FFad3 <gnebapatryk@gmail.com>
2024-08-30 16:40:54 +0200

    Add file size formatter extension

---
+++ ToyParser.Utils/FileSizeExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace ToyParser.Utils
+{
+    public static class FileSizeExtension
+    {
+        public static string FormatFileSizeAsString(long bytes)
+        {
+            const long kilobyte = 1024;
+            const long megabyte = kilobyte * 1024;
+            const long gigabyte = megabyte * 1024;
+            const long terabyte = gigabyte * 1024;
+
+            if (bytes >= terabyte)
+            {
+                return $"{bytes / (double)terabyte:F2} TB";
+            }
+            else if (bytes >= gigabyte)
+            {
+                return $"{bytes / (double)gigabyte:F2} GB";
+            }
+            else if (bytes >= megabyte)
+            {
+                return $"{bytes / (double)megabyte:F2} MB";
+            }
+            else if (bytes >= kilobyte)
+            {
+                return $"{bytes / (double)kilobyte:F2} KB";
+            }
+            else
+            {
+                return $"{bytes} Bytes";
+            }
+        }
+    }
+}
