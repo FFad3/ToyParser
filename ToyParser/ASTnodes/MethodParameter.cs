2ca64ccf0c0cf2e0ee7b584bd656a9d745c940aa
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:49:32 +0200

    Add AST MethodParameter

---
+++ ToyParser/ASTnodes/MethodParameter.cs
@@ -0,0 +1,19 @@
+﻿using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class MethodParameter(VariableType Type, Identifier Identifier) : ASTNode
+    {
+        public VariableType Type { get; } = Type;
+        public Identifier Identifier { get; } = Identifier;
+
+        public override void Accept(IASTVisitor visitor, bool isLast)
+        {
+            visitor.Visit(this, isLast);
+        }
+
+        public override string GetValue() => Type.ToString();
+
+        public override IEnumerable<ASTNode> GetChildren() => [Identifier];
+    }
+}
