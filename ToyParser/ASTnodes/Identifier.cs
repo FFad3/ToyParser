089868af3aa33534f4f3fe69333b4bdc9182510f
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:49:08 +0200

    Add AST Identifier

---
+++ ToyParser/ASTnodes/Identifier.cs
@@ -0,0 +1,11 @@
+﻿namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class Identifier(string value) : ASTNode
+    {
+        public string Value { get; } = value;
+
+        public override IEnumerable<ASTNode> GetChildren() => [];
+
+        public override string GetValue() => Value;
+    }
+}
