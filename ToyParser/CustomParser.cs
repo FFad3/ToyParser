f24cdb53ff9a6b2eee37f43ee2017944a186b2fa
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:50:31 +0200

    Add Parser logic supporting variable and methods declarations

--- ToyParser/CustomParser.cs
+++ ToyParser/CustomParser.cs
@@ -49,7 +49,114 @@ namespace ToyParser.Parser
 
         private ASTNode ParseStatement()
         {
-            return ParseExpression();
+            return _currentToken.Type switch
+            {
+                TokenType.INT => ParseVariableOrMethodDeclaration(ParseIntType()),
+                TokenType.STRING => ParseVariableOrMethodDeclaration(ParseStringType()),
+                TokenType.BOOLEAN => ParseVariableOrMethodDeclaration(ParseBooleanType()),
+                TokenType.RETURN => ParseReturnStatement(),
+                _ => throw new SyntaxErrorException($"Unexpected token {_currentToken}")
+            };
+        }
+
+        private VariableDeclarator ParseVariableDeclarator(bool isArray)
+        {
+            var identifier = ParseIdentifier();
+            var equalsValueClause = ParseEqualsValueClause(isArray);
+            return new VariableDeclarator(identifier, equalsValueClause);
+        }
+
+        private EqualsValueClause ParseEqualsValueClause(bool isArray)
+        {
+            Eat(TokenType.ASSIGN);
+            ASTNode node;
+            if (isArray)
+            {
+                node = ParseArrayIndexCall();
+            }
+            else
+            {
+                node = ParseExpression();
+            }
+            return new EqualsValueClause(node);
+        }
+
+        private ASTNode ParseVariableOrMethodDeclaration(VariableType variableType)
+        {
+            Identifier identifier = ParseIdentifier();
+            return _currentToken.Type switch
+            {
+                TokenType.ASSIGN => ParseVariableDeclaracion(variableType, identifier),
+                TokenType.L_PAREN => ParseMethodDeclaracion(variableType, identifier),
+                _ => throw new SyntaxErrorException($"Unexpected token {_currentToken}")
+            };
+        }
+
+        private ASTNode ParseVariableDeclaracion(VariableType variableType, Identifier identifier)
+        {
+            var variableDeclarators = new List<VariableDeclarator>();
+            //Process first declarator because we need picked first identifier to recognize if its method or variable declaration
+            var declarator = new VariableDeclarator(identifier, ParseEqualsValueClause(variableType.IsArray));
+            variableDeclarators.Add(declarator);
+
+            while (_currentToken.Type != TokenType.SEMICOLON)
+            {
+                var variableDeclarator = ParseVariableDeclarator(variableType.IsArray);
+                variableDeclarators.Add(variableDeclarator);
+
+                if (_currentToken.Type == TokenType.COMMA)
+                    Eat(TokenType.COMMA);
+            }
+            Eat(TokenType.SEMICOLON);
+            return new VariableDeclaration(variableType, variableDeclarators);
+        }
+
+        private ASTNode ParseMethodDeclaracion(VariableType variableType, Identifier identifier)
+        {
+            Eat(TokenType.L_PAREN);
+            List<MethodParameter> methodParameters = new List<MethodParameter>();
+            while (_currentToken.Type != TokenType.R_PAREN)
+            {
+                var methodParameter = ParseMethodParameter();
+                methodParameters.Add(methodParameter);
+
+                if (_currentToken.Type == TokenType.COMMA)
+                    Eat(TokenType.COMMA);
+            }
+            Eat(TokenType.R_PAREN);
+
+            BlockStatement blockStatement = ParseBlockStatement();
+            return new MethodDeclaration(variableType, identifier, methodParameters, blockStatement);
+        }
+
+        private BlockStatement ParseBlockStatement()
+        {
+            Eat(TokenType.L_BRACE);
+            ReturnStatement returnStatement = null!;
+            List<ASTNode> statements = new List<ASTNode>();
+
+            do
+            {
+                var statement = ParseStatement();
+                if (statement is ReturnStatement rStatement)
+                {
+                    returnStatement = rStatement;
+                }
+                else
+                {
+                    statements.Add(statement);
+                }
+            } while (returnStatement is null);
+            Eat(TokenType.R_BRACE);
+            return new BlockStatement(statements, returnStatement);
+        }
+
+        private ReturnStatement ParseReturnStatement()
+        {
+            Eat(TokenType.RETURN);
+            var expression = ParseExpression();
+            Eat(TokenType.SEMICOLON);
+            return new ReturnStatement(expression);
         }
 
         private ASTNode ParseExpression()
@@ -75,6 +182,7 @@ namespace ToyParser.Parser
 
             return left;
         }
+
         private ASTNode ParsePrimary()
         {
             return _currentToken.Type switch
@@ -82,6 +190,7 @@ namespace ToyParser.Parser
                 TokenType.DIGIT => ParseNumericLiteral(),
                 TokenType.STRING_LITERAL => ParseStringLiteral(),
                 TokenType.L_PAREN => ParseInneBinaryExpression(),
+                TokenType.IDENTIFIER => ParseIdentifier(),
                 _ => throw new SyntaxErrorException($"Unexpected token {_currentToken}")
             };
         }
@@ -92,6 +201,63 @@ namespace ToyParser.Parser
             var exp = ParseBinaryExpression();
             Eat(TokenType.R_PAREN);
             return exp;
+        }
+
+        private MethodParameter ParseMethodParameter()
+        {
+            VariableType variableType;
+            switch (_currentToken.Type)
+            {
+                case TokenType.INT:
+                    variableType = ParseVariableType(TokenType.INT);
+                    break;
+
+                case TokenType.STRING:
+                    variableType = ParseVariableType(TokenType.STRING);
+                    break;
+
+                case TokenType.BOOLEAN:
+                    variableType = ParseVariableType(TokenType.BOOLEAN);
+                    break;
+
+                default:
+                    throw new SyntaxErrorException($"Unexpected token {_currentToken}");
+            }
+            Identifier identifier = ParseIdentifier();
+            return new MethodParameter(variableType, identifier);
+        }
+
+        private Identifier ParseIdentifier()
+        {
+            var identifier = new Identifier(_currentToken.Value);
+            Eat(TokenType.IDENTIFIER);
+            return identifier;
+        }
+
+        private ArrayIndexCall ParseArrayIndexCall()
+        {
+            Eat(TokenType.L_BRACKET);
+            var numericliteral = ParseNumericLiteral();
+            Eat(TokenType.R_BRACKET);
+            return new ArrayIndexCall(numericliteral);
+        }
+
+        private VariableType ParseIntType() => ParseVariableType(TokenType.INT);
+
+        private VariableType ParseStringType() => ParseVariableType(TokenType.STRING);
+
+        private VariableType ParseBooleanType() => ParseVariableType(TokenType.BOOLEAN);
+
+        private VariableType ParseVariableType(TokenType tokenType)
+        {
+            Eat(tokenType);
+            if (_currentToken.Type == TokenType.L_BRACKET)
+            {
+                Eat(TokenType.L_BRACKET);
+                Eat(TokenType.R_BRACKET);
+                return new VariableType(tokenType, _currentToken.Value, true);
+            }
+            return new VariableType(tokenType, _currentToken.Value, false);
         }
 
         private StringLiteral ParseStringLiteral()
