1cf3b4ebb6541fbf8ab6218c26af0aa4bb979f08
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:50:09 +0200

    Add AST BlockStatement

---
+++ ToyParser/ASTnodes/BlockStatement.cs
@@ -0,0 +1,25 @@
+﻿using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class BlockStatement(IEnumerable<ASTNode> statements, ReturnStatement returnStatement) : ASTNode
+    {
+        private readonly List<ASTNode> _statements = statements.ToList();
+        public ReturnStatement ReturnStatement { get; } = returnStatement;
+
+        public override void Accept(IASTVisitor visitor, bool isLast)
+        {
+            visitor.Visit(this, isLast);
+        }
+
+        public override string GetValue() => string.Empty;
+
+        public override IEnumerable<ASTNode> GetChildren()
+        {
+            var nodes = new List<ASTNode>();
+            nodes.AddRange(_statements);
+            nodes.Add(ReturnStatement);
+            return nodes;
+        }
+    }
+}
