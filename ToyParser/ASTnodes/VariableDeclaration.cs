ebf81f1f53a0d37ececb1cae485db0be4f998a6f
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:49:43 +0200

    Add AST VariableDeclaration

---
+++ ToyParser/ASTnodes/VariableDeclaration.cs
@@ -0,0 +1,19 @@
+﻿using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class VariableDeclaration(VariableType Type, List<VariableDeclarator> declarators) : ASTNode
+    {
+        public VariableType Type { get; } = Type;
+        private List<VariableDeclarator> _declarators = declarators;
+
+        public override void Accept(IASTVisitor visitor, bool isLast)
+        {
+            visitor.Visit(this, isLast);
+        }
+
+        public override string GetValue() => $"{Type}";
+
+        public override IEnumerable<ASTNode> GetChildren() => _declarators;
+    }
+}
