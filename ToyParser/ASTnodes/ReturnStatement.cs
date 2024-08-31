54bf48f4bb3ac7945b980285c32073b178e42850
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:50:00 +0200

    Add AST ReturnStatement

---
+++ ToyParser/ASTnodes/ReturnStatement.cs
@@ -0,0 +1,18 @@
+﻿using ToyParser.Parser.Visitor;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public sealed class ReturnStatement(ASTNode expression) : ASTNode
+    {
+        public ASTNode Expression { get; } = expression;
+
+        public override void Accept(IASTVisitor visitor, bool isLast)
+        {
+            visitor.Visit(this, isLast);
+        }
+
+        public override string GetValue() => string.Empty;
+
+        public override IEnumerable<ASTNode> GetChildren() => [Expression];
+    }
+}
