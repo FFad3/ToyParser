64c4b93a383a06c5874f647e04df89330652b09f
FFad3 <gnebapatryk@gmail.com>
2024-08-31 19:44:58 +0200

    Add VariableType

---
+++ ToyParser/ASTnodes/VariableType.cs
@@ -0,0 +1,20 @@
+﻿using ToyParser.Lexer;
+
+namespace ToyParser.Parser.ASTnodes
+{
+    public readonly struct VariableType
+    {
+        public TokenType Type { get; }
+        public bool IsArray { get; }
+        public string Value { get; }
+
+        public VariableType(TokenType type, string value,bool isArray)
+        {
+            Type = type;
+            Value = value;
+            IsArray = isArray;
+        }
+
+        public override string ToString() => $"{Type.ToString()}{(IsArray ? "[]":string.Empty)}";
+    }
+}
